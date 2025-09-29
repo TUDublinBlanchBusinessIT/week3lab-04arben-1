fbad74feb5b5b6947cb7db38009e3206d49e93f8
04arben <b00168185@mytudublin.ie>
2025-09-29 16:18:28 +0100

    Finished the second part

--- week3-lab/App.js
+++ week3-lab/App.js
@@ -4,12 +4,20 @@ import Logo from './components/Logo';
 
 export default function App() {
   const [fullname] = useState("Eoin");
+  const [fname, setFname] = useState("Joe");
+  const [lname, setLname] = useState("Bloggs");
+  const [dob, setDob] = useState("22 August 1990");
   return(
     <View>
       <Logo/>
 
       <Text>Hello, World {fullname}</Text>
+      <Text>Hello {fname} {lname}. You were born on {dob}</Text>
       <TextInput placeholder="enter your name" onChangeText={fullname}/>
+      <TextInput placeholder="Enter your firstname" onChangeText={setFname}/>
+      <TextInput placeholder="Enter your lastname" onChangeText={setLname}/>
+      <TextInput placeholder="Enter your date of birth" onChangeText={setDob}/>
     </View>
   )
 };
+
